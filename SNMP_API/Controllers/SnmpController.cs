6afe75574f1571d4fe44e60e4b4669bea427b83d
VINICIUS MULLER TEIXEIRA <16204146@pucrs.br>
2018-10-16 18:25:47 -0300

    SnpmController, start entry point

--- SNMP_API/Controllers/SnmpController.cs
+++ SNMP_API/Controllers/SnmpController.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SnmpSharpNet;
+using SNMP_API.Models;
 
 namespace SNMP_API.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class SnmpController : Controller
     {
-        // GET api/values
+        // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+        /*
+         * Ponto de Entreda:
+         * recebe a Community e Endereço do Agente a ser Monitorado
+         * deve iniciar todos os serviços e libera o uso de outras rotas
+         * para o front end consumir
+        */
+        [HttpPost("start")]
+        public IActionResult Start([FromBody] Setting setting)
+        {
+            return Json(setting);
         }
 
         // GET api/values/5
---
+++ SNMP_API/Models/Setting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNMP_API.Models
+{
+    public class Setting
+    {
+        public string Community { get; set; }
+        public string HostAddress { get; set; }
+    }
+}
